a1f69bc0e92624a447f041fbade7a205c72b617e
anna2678 <83779965+anna2678@users.noreply.github.com>
2021-05-11 18:22:25 +0300

    2

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 window.onload = function(){
+
+    function showDate(){
+        var str = '';
+        var now = new Date();
+        str = now.toLocaleTimeString();
+        document.querySelector('.date').innerHTML = str;
+    }
+    setInterval(showDate, 1000);
     $.getJSON("http://spreadsheets.google.com/feeds/list/10H4WyiohNqiMSaw-94vAwDJeyjS05nXab_AfvUv38wg/od6/public/values?alt=json",
     function(data){
        data = data['feed']['entry']
@@ -14,9 +22,12 @@ function show (data){
        out += `<h3 class="title">${data[i]['gsx$name']['$t']}</h3>`;
        out +=   `<img src="${data[i]['gsx$image']['$t']}">`;
        out += `  <p class="price">цена:${data[i]['gsx$price']['$t']}</p>`;
-       out+=`</div>` ;
+       out+=`<button name="add_to_cart" data =" ${data[i]['gsx$article']['$t']}">купить</button>` ;
        out+=`</div>`;
     }
     document.querySelector('.product_field').innerHTML = out;
+}
+document.onclick = function(e){
+    console.log(e.target.attributes.name.nodeValue,e.target.attributes.data.nodeValue);
 }
 }
